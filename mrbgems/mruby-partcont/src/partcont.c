3f1a344312e4e1d25e4de9fcc12827a4b72601da
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-09-28 10:50:51 +0900

    add Kernel.#shift, but does not support continuation recalls yet

--- mrbgems/mruby-partcont/src/partcont.c
+++ mrbgems/mruby-partcont/src/partcont.c
@@ -68,11 +68,70 @@ mrb_kernel_reset(mrb_state* mrb, mrb_value self)
   return c->ci->proc->env->stack[0];
 }
 
+static mrb_value
+mrb_kernel_shift(mrb_state* mrb, mrb_value self)
+{
+  static const struct mrb_context mrb_context_zero = { 0 };
+  struct mrb_context *c;
+  struct RProc *p;
+  mrb_callinfo *ci;
+  mrb_value blk;
+
+  mrb_get_args(mrb, "&", &blk);
+
+  if (mrb_nil_p(blk)) {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "tried to shift without a block");
+  }
+  p = mrb_proc_ptr(blk);
+  if (MRB_PROC_CFUNC_P(p)) {
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "tried to shift from C defined method");
+  }
+
+  c = (struct mrb_context*)mrb_malloc(mrb, sizeof(struct mrb_context));
+  *c = mrb_context_zero;
+
+  /* initialize VM stack */
+  c->stbase = (mrb_value *)mrb_calloc(mrb, FIBER_STACK_INIT_SIZE, sizeof(mrb_value));
+  c->stend = c->stbase + FIBER_STACK_INIT_SIZE;
+  c->stack = c->stbase;
+
+  /* copy receiver from a block */
+  c->stack[0] = mrb->c->stack[0];
+
+  /* initialize callinfo stack */
+  c->cibase = (mrb_callinfo *)mrb_calloc(mrb, FIBER_CI_INIT_SIZE, sizeof(mrb_callinfo));
+  c->ciend = c->cibase + FIBER_CI_INIT_SIZE;
+  c->ci = c->cibase;
+
+  /* adjust return callinfo */
+  ci = c->ci;
+  ci->target_class = p->target_class;
+  ci->proc = p;
+  ci->pc = p->body.irep->iseq;
+  ci->nregs = p->body.irep->nregs;
+  ci[1] = ci[0];
+  c->ci++;                      /* push dummy callinfo */
+
+  c->fib = NULL;
+
+  /* delegate process */
+  mrb->c->status = MRB_FIBER_RESUMED;
+  c->cibase->argc = 0;		/* block with no args */
+  c->prev = mrb->c->prev;
+  c->status = MRB_FIBER_RUNNING;
+  mrb->c = c;
+
+  MARK_CONTEXT_MODIFY(c);
+  return c->ci->proc->env->stack[0];
+}
+
 void
 mrb_mruby_partcont_gem_init(mrb_state* mrb)
 {
   mrb_define_class_method(mrb, mrb->kernel_module, "reset", mrb_kernel_reset, MRB_ARGS_BLOCK());
   mrb_define_method(mrb, mrb->kernel_module, "reset", mrb_kernel_reset, MRB_ARGS_BLOCK());
+  mrb_define_class_method(mrb, mrb->kernel_module, "shift", mrb_kernel_shift, MRB_ARGS_BLOCK());
+  mrb_define_method(mrb, mrb->kernel_module, "shift", mrb_kernel_shift, MRB_ARGS_BLOCK());
 }
 
 void
